0a1062849dbcfb3d6aa94539c32f960b11464de9
martinvibes <martinmachiebe21@gmail>
2026-01-27 22:18:48 +0100

    updates

--- contract/contracts/hello-world/src/base/errors.rs
+++ contract/contracts/hello-world/src/base/errors.rs
@@ -2,7 +2,7 @@ use soroban_sdk::contracterror;
 
 #[contracterror]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-#[repr(u32)] // This is required for Soroban errors
+#[repr(u32)] // This is required for most Soroban errors 
 pub enum Error {
     InvalidInput = 1,
     AlreadyExists = 2,
